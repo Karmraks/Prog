97fab20d75be0bc9c1a9f862a5f2788f6f4f1e9b
V1RTEX <oleg04011999@gmail.com>
2019-12-15 15:04:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+//let arr = ["plum.png", "orange.jpg", "apple.jpg"]
+
+//alert('Hello world!');
+
+
+// let answer = confirm("Are you here");
+
+// console.log(answer);
+
+//let answer = prompt("Do you have 18", "Yes");
+
+//console.log(typeof(answer));
+
+// let incr = 10, 
+    // decr = 10;
+    // incr++;
+    // decr--;
+    // console.log(incr);
+    // console.log(decr);
+
+// let isChecked = true,
+//     isClose = true;
+
+//  console.log(isChecked && isClose);
+
+    //alert("Привет это моя первая страница");
+
+//     let luda = "world",
+//     	kolya = "100";
+//     let result = '';	
+// alert(result);
+// console.log(typeof([]));
+// console.log(2 ** 3 ** 2);
+var s = 'mystring';
+for (var i = 0; i < s.length; i++) {
+ console.log(s.charAt(i));
+}
